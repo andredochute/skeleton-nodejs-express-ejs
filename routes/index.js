abfb9d979321f8874c06a77690b0c981f203683e
André Lopes <86085474+andredochute@users.noreply.github.com>
2022-03-16 20:23:38 +0000

    Fizemos uma função inserir

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 const Autor =require("../models/autor");
 
-router.get("/", async function(req, res, netx){
+router.get("/", async function(req, res, next){
   const autores = await Autor.selecionar();
   res.json(autores.rows);
 });
 
-
+router.get("/inserir", async function(req, res, netx){
+  const autor = {
+    nome: "André",
+    sobrenome: "lopes",
+    data_nascimento: "2005/03/02"
+  }
+  const autores = await Autor.inserir(autor);
+  res.json(autores);
+});
 
 
 module.exports = router;
